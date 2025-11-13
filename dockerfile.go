bc92c77c8dddf39bd551bc489d5c84fb5798a7f9
Muhammad Is'ad Prabaswara <81808543+ovdsd@users.noreply.github.com>
2025-11-13 23:26:41 +0700

    fix: Dockerfile dont run all of the required services
    Updated Dockerfile to build multiple binaries and adjust entry point.

--- dockerfile.go
+++ dockerfile.go
@@ -7,16 +7,24 @@ RUN go mod download
 
 COPY . .
 
-RUN CGO_ENABLED=0 go build -o /app/server ./cmd/gateway
+# Build all binaries
+RUN CGO_ENABLED=0 go build -o /app/bin/gateway ./cmd/gateway && \
+    CGO_ENABLED=0 go build -o /app/bin/user ./cmd/services/user && \
+    CGO_ENABLED=0 go build -o /app/bin/commissions ./cmd/services/commissions && \
+    CGO_ENABLED=0 go build -o /app/bin/pos ./cmd/services/pos && \
+    CGO_ENABLED=0 go build -o /app/bin/inventory ./cmd/services/inventory
 
+# Runtime image
 FROM alpine:latest
+
+RUN apk add --no-cache tini
 
 WORKDIR /app
 
 COPY --from=builder /app/config/ ./config/
-
-COPY --from=builder /app/server .
+COPY --from=builder /app/bin/ ./bin/
 
 EXPOSE 8080
 
-CMD ["./server"]
+ENTRYPOINT ["/sbin/tini", "--"]
+CMD ["sh", "-c", "./bin/user & ./bin/commissions & ./bin/pos & ./bin/inventory & ./bin/gateway"]
