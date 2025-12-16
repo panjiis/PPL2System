425fa77a5fc1446d084ceb925547f9c0ac2c2e01
EvanAhnafSatyatama <evan22002@mail.unpad.ac.id>
2025-12-16 20:32:51 +0700

    debug: add debugging on pubsub analytics

--- analytics_service/pubsub_subscriber.py
+++ analytics_service/pubsub_subscriber.py
@@ -385,6 +385,8 @@ async def message_handler(msg):
                         "cost_price": Decimal(cost_price_str)
                     })
 
+                print(f"new item : \n {new_items}")
+                
                 if new_items:
                     db.execute(text("DELETE FROM raw_order_items WHERE document_number = :doc_num"), {"doc_num": doc_number})
                     db.bulk_insert_mappings(RawOrderItem, new_items)
